c372fea269b092cba0e29984232cde1975525f07
Mathew Nieves <148835092+MatNvs@users.noreply.github.com>
2023-12-10 18:04:42 -0800

    Delete public/src/src directory

--- public/src/src/helper.js
+++
@@ -1,4 +0,0 @@
-// function that will take authors array and author ID, and return author object.
-const grabAuthorById = (authors, id) => {
-    return authors.find((author) => author.id === id);
-};
